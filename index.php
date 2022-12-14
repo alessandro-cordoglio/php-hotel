3e30c5949df63539b128d0bcdd31b86de1954fb4
alessandro-cordoglio <alessandro.cord20@gmail.com>
2022-12-14 18:55:27 +0100

    inseriti i dati all'interno di una tabella

--- index.php
+++ index.php
@@ -48,20 +48,33 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
     <title>Hotel</title>
 </head>
 <body>
     <h1>Lista hotel:</h1>
-    <ul>
-        <?php for($i = 0; $i < count($hotels); $i++){ ?>
-        <li>
-            <p><?php echo $hotels[$i]['name']; ?></p> 
-            <p><?php echo $hotels[$i]['description']; ?></p> 
-            <p><?php echo $hotels[$i]['parking']; ?></p> 
-            <p><?php echo $hotels[$i]['vote']; ?></p> 
-            <p><?php echo$hotels[$i]['distance_to_center'];?> </p> 
-        </li>   
-        <?php }?>  
-    </ul>
+    <table>
+        <thead>
+            <tr>
+                <th scope="col">Nome</th>
+                <th scope="col">Descrizione</th>
+                <th scope="col">Parcheggio</th>
+                <th scope="col">Votazione</th>
+                <th scope="col">Distanza dal centro</th>
+            </tr>
+        </thead>
+        <tbody>
+            <?php for($i = 0; $i < count($hotels); $i++){ ?>
+            <tr>
+                    <th scope="row"><?php echo $hotels[$i]['name']?></th>
+                    <td><?php echo $hotels[$i]['description']?></td>
+                    <td><?php echo $hotels[$i]['parking']?></td>
+                    <td><?php echo $hotels[$i]['vote']?></td>
+                    <td><?php echo $hotels[$i]['distance_to_center']?></td>
+            </tr>
+            <?php }?>  
+        </tbody>    
+        
+    </table>
 </body>
 </html>
